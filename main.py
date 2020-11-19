359a026f44f421dd006173360547e5414eebe7a5
Hamza Krika <63921551+Hemzyy@users.noreply.github.com>
2020-11-19 19:15:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import requests, os, sys
-from random import randint
+import requests
+
+def downloadThumbnail(url):
+    id = url.split('=',1)[1]
+    thumbUrl = 'https://img.youtube.com/vi/'+ id + '/maxresdefault.jpg'
+    path = './thumbnail' + id + '.jpg'
+    thumb = requests.get(thumbUrl)
+    with open(path, 'wb') as f:
+        f.write(thumb.content)
 
 url = input('Enter video url that you want to download the thumbnail of: ')
-id = url.split('=',1)[1]
-thumbUrl = 'https://img.youtube.com/vi/'+ id + '/maxresdefault.jpg'
-
 print("Downloading . . .")
-
-name = str(randint(0, 999))
-path = './thumbnail' + name + '.jpg'
-thumb = requests.get(thumbUrl)
-with open(path, 'wb') as f:
-    f.write(thumb.content)
-
+downloadThumbnail(url)
 print('Thumbnail downloaded!')
